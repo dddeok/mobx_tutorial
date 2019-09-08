38b9118990852f0f28ffca6a99724bbc8b61b602
dddeok <crew0813@gmail.com>
2019-09-08 16:13:29 +0900

    yarn eject

---
+++ src/store/counter.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { observable, action, decorate } from 'mobx';
+import { observer } from 'mobx-react';
+
+class CounterStore extends Component {
+    number = 0;
+
+    increase = () => {
+        this.number++;
+    }
+
+    
+    decrease = () => {
+        this.number--;
+    }
+}
+
+decorate(CounterStore, {
+    number : observable,
+    increase : action,
+    decrease : action
+})
+
+export default observer(CounterStore);
